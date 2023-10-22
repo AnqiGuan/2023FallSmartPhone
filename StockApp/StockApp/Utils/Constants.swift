ba2050f5976ac3879742f30e7647af923dfc323e
Anqi Guan <63136314+AnqiGuan@users.noreply.github.com>
2023-10-21 17:31:32 -0700

    Committing the code for stack app

---
+++ StockApp/StockApp/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockApp
+//
+//  Created by Anqi Guan on 10/21/23.
+//
+
+import Foundation
+
+let apiKey = "f7c40c564b46837fef8ba9102cbe4418"
+let baseURL = "https://financialmodelingprep.com/api/v3/"
